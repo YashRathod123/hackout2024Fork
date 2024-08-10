6942191213023e4c36e1eefb0530289991fda76a
Om-Patel1705 <omupatel1705@gmail.com>
2024-08-10 18:50:44 +0530

    Final2ndSub

--- Client/src/Components/card.jsx
+++ Client/src/Components/card.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import "../index.scss";
-import { Card, CardHeader, CardBody, CardFooter,Image ,Stack,Heading,Text,Divider,ButtonGroup,Button} from '@chakra-ui/react';
+import { Card, CardHeader, CardBody, CardFooter,Image ,Stack,Heading,Text,Divider,ButtonGroup,Button, Box} from '@chakra-ui/react';
 function Cardi({data1}){
 
   // const data = props.json();
 
 
 
-    return (<div>
+    return (<Box width="100%" height="100%">
         <Card maxW='sm' className="card">
   <CardBody>
     <Image
@@ -31,6 +31,6 @@ function Cardi({data1}){
     </ButtonGroup>
   </CardFooter>
 </Card>
-    </div>);
+    </Box>);
 }
  export default Cardi;
